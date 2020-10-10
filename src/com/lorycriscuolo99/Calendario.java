e1b6469e5a90b5f3847b18993c9557bbbfd991f9
xXViRtUaLcObRaXx <65489866+xXViRtUaLcObRaXx@users.noreply.github.com>
2020-10-10 15:47:01 +0200

    Update Calendario.java

--- src/com/lorycriscuolo99/Calendario.java
+++ src/com/lorycriscuolo99/Calendario.java
@@ -42,8 +42,7 @@ public class Calendario extends GregorianCalendar{
      * @return the value saved in the DAY_OF_MONTH field
      */
     public int getDayOfMonth(){
-        //utile
-        System.out.println("Utilissimo!");
+   
          return super.get(GregorianCalendar.DAY_OF_MONTH);
     }
     
